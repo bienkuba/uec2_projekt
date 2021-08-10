b3b67c3017ef2fcca0aed2cbe427b9e38321d16d
bienkuba <bienkuba@interia.pl>
2021-08-10 17:19:38 +0200

    time to write!
    file char_rom_16x16.v added
    file draw_rect_char.v added
    file font_rom.v added
    a lot of warnings added

---
+++ vivado/rtl/draw_rect_char.v
@@ -0,0 +1,104 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 13.04.2021 13:15:24
+// Design Name: 
+// Module Name: draw_rect_char
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module draw_rect_char(
+ input wire rst,
+ input wire pclk,
+ 
+ input wire [10:0] vcount_in,
+ input wire vsync_in,
+ input wire vblnk_in,
+ input wire [10:0] hcount_in,
+ input wire hsync_in,
+ input wire hblnk_in,
+ input wire [11:0] rgb_in,
+ input wire [7:0] char_pixels,
+ 
+ output reg [10:0] vcount_out,
+ output reg vsync_out,
+ output reg vblnk_out,
+ output reg [10:0] hcount_out,
+ output reg hsync_out,
+ output reg hblnk_out,
+ output reg [11:0] rgb_out,
+ output reg [7:0]  char_xy, 
+ output reg [3:0]  char_line
+// output reg [10:0] addr
+ );
+ 
+ localparam WIDTH  = 48;
+ localparam HEIGHT = 64;
+ localparam COLOR  = 12'h0_9_9;
+
+ localparam BACKGROUND = 12'h3_3_f; 
+ localparam LETTERS_COLOR = 12'ha_b_c;
+ localparam XPOS = 300, YPOS = 100;
+ 
+ reg [10:0] addrx, addry;
+ reg [11:0] rgb_out_nxt;
+          
+ always@(posedge pclk)begin
+   if (rst) begin
+     rgb_out    <= 0;
+     hsync_out  <= 0;
+     vsync_out  <= 0;
+     hblnk_out  <= 0;
+     vblnk_out  <= 0;          
+     hcount_out <= 0;
+     vcount_out <= 0;
+   end
+   else begin
+     hsync_out  <= hsync_in;
+     vsync_out  <= vsync_in;
+     hblnk_out  <= hblnk_in;
+     vblnk_out  <= vblnk_in;  
+     hcount_out <= hcount_in;
+     vcount_out <= vcount_in;
+     rgb_out    <= rgb_out_nxt;
+   end
+ end 
+         
+  always @*begin
+  // During blanking, paint it black. 
+    if (vblnk_in || hblnk_in) rgb_out_nxt = 12'h0_0_0; 
+    
+    else begin
+      if ((vcount_in >= YPOS && vcount_in <= 256 + YPOS) && (hcount_in >= XPOS && hcount_in <= 128 + XPOS))begin
+        if (char_pixels[4'b1000-addrx[2:0]])
+          rgb_out_nxt = LETTERS_COLOR;
+        else
+          rgb_out_nxt = BACKGROUND;
+        end
+      else
+        rgb_out_nxt = rgb_in;
+      end
+    end
+ 
+   always@* begin
+     addry     = vcount_in - YPOS;
+     addrx     = hcount_in - XPOS;
+     char_xy   = {addry[7:4], addrx[6:3]};
+     char_line = addry[3:0];
+   end   
+    
+    
+endmodule  
--- vivado/rtl/vga_example.v
+++ vivado/rtl/vga_example.v
@@ -1,14 +1,4 @@
-// File: vga_example.v
-// This is the top level design for EE178 Lab #4.
-
-// The `timescale directive specifies what the
-// simulation time units are (1 ns here) and what
-// the simulator time step should be (1 ps here).
-
 `timescale 1 ns / 1 ps
-
-// Declare the module and its ports. This is
-// using Verilog-2001 syntax.
 
 module vga_example (
   input wire clk,
@@ -21,9 +11,6 @@ module vga_example (
   output wire pclk_mirror
   );
 
-  // Converts 100 MHz clk into 40 MHz pclk.
-  // This uses a vendor specific primitive
-  // called MMCME2, for frequency synthesis.
 
   wire clk_in;
   wire locked;
@@ -35,41 +22,6 @@ module vga_example (
   (* KEEP = "TRUE" *) 
   (* ASYNC_REG = "TRUE" *)
   reg [7:0] safe_start = 0;
-  /*
-  IBUF clk_ibuf (.I(clk),.O(clk_in));
-
-  MMCME2_BASE #(
-    .CLKIN1_PERIOD(10.000),
-    .CLKFBOUT_MULT_F(10.000),
-    .CLKOUT0_DIVIDE_F(25.000))
-  clk_in_mmcme2 (
-    .CLKIN1(clk_in),
-    .CLKOUT0(clk_out),
-    .CLKOUT0B(),
-    .CLKOUT1(),
-    .CLKOUT1B(),
-    .CLKOUT2(),
-    .CLKOUT2B(),
-    .CLKOUT3(),
-    .CLKOUT3B(),
-    .CLKOUT4(),
-    .CLKOUT5(),
-    .CLKOUT6(),
-    .CLKFBOUT(clkfb),
-    .CLKFBOUTB(),
-    .CLKFBIN(clkfb),
-    .LOCKED(locked),
-    .PWRDWN(1'b0),
-    .RST(1'b0)
-  );
-
-  BUFH clk_out_bufh (.I(clk_out),.O(clk_ss));
-  always @(posedge clk_ss) safe_start<= {safe_start[6:0],locked};
-
-  BUFGCE clk_out_bufgce (.I(clk_out),.CE(safe_start[7]),.O(pclk));
-  */
-  // Mirrors pclk on a pin for use by the testbench;
-  // not functionally required for this design to work.
 
   ODDR pclk_oddr (
     .Q(pclk_mirror),
@@ -93,12 +45,19 @@ module vga_example (
       .locked(locked)
     );
 
-  wire [10:0] vcount, hcount, hcount_out_b, vcount_out_b, hcount_out_r, vcount_out_r;
-  wire vsync, hsync, vsync_out_b, hsync_out_b, vsync_out_r, hsync_out_r;
-  wire vblnk, hblnk, vblnk_out_b, hblnk_out_b, vblnk_out_r, hblnk_out_r;
-  wire [11:0] rgb_out_b, rgb_out_r;
-
-
+  
+  wire vsync, hsync, vsync_out_b, hsync_out_b, vsync_out_r, hsync_out_r, vblnk, hblnk, 
+       vblnk_out_b, hblnk_out_b, vblnk_out_r, hblnk_out_r, vblnk_delay, hblnk_delay, 
+       vblnk_out_ch, hblnk_out_ch, vblnk_delay_ch, hblnk_delay_ch, vsync_delay, hsync_delay, 
+       vsync_delay_r, hsync_delay_r, vsync_out_ch, hsync_out_ch, hsync_delay_th, vsync_delay_th, 
+       hsync_delay_ch, vsync_delay_ch;
+  
+  wire [3:0] char_line;
+  wire [6:0] char_code;
+  wire [7:0] char_pixels, char_xy;
+  wire [10:0] vcount, hcount, hcount_out_b, vcount_out_b, hcount_out_r, vcount_out_r, hcount_out_ch, vcount_out_ch;
+  wire [11:0] rgb_out_b, rgb_out_r, rgb_out_ch;
+  
   vga_timing my_timing (
     .vcount(vcount),
     .rst(rst),
@@ -149,12 +108,48 @@ module vga_example (
     .vblnk_out(vblnk_out_r),
     .rgb_out(rgb_out_r)
     );
-      
+
+draw_rect_char my_draw_rect_char (
+    .rst(rst),
+    .pclk(pclk),
+    
+    .vcount_in(vcount_out_r),
+    .vsync_in(vsync_out_r),
+    .vblnk_in(vblnk_out_r),
+    .hcount_in(hcount_out_r),
+    .hsync_in(vsync_out_r),
+    .hblnk_in(hblnk_out_r),   
+    .rgb_in(rgb_out_r),
+    .char_pixels(char_pixels),
+           
+    .vcount_out(vcount_out_ch),
+    .vsync_out(vsync_delay_r),
+    .vblnk_out(vblnk_out_ch),
+    .hcount_out(hcount_out_ch),
+    .hsync_out(hsync_delay_r),
+    .hblnk_out(hblnk_out_ch),
+    .char_line(char_line),  
+    .char_xy(char_xy),
+    .rgb_out(rgb_out_ch)   
+  );      
+
+  font_rom my_font_rom(
+    .clk(pclk),
+    .addr({char_code, char_line}),
+          
+    .char_line_pixels(char_pixels)
+  );
+  
+  char_rom_16x16 my_char_rom(
+    .char_xy(char_xy), 
+    .char_code(char_code) 
+  );      
   always @(posedge pclk)begin
     hs <= hsync_out_r;
     vs <= vsync_out_r;
     {r,g,b} <= rgb_out_b;
     {r,g,b} <= rgb_out_r;
+    {r,g,b} <= rgb_out_ch;
    end
 
 endmodule
