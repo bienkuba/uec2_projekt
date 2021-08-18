02329d5090fb1ce6eaec1ec2e2acebc5febd23cb
BKlymchuk <bohdan@reportgarden.com>
2021-08-18 19:26:07 +0200

    fix: debounce attempt 3

--- vivado/rtl/debounce_r.v
+++ vivado/rtl/debounce_r.v
@@ -4,15 +4,13 @@ module debounce_r (
     output rect_right
 );
     wire clk_out;
-    wire Q;
-    wire Q_n;
-    wire Q_bar;
+    wire Q, Q_n, Q_bar;
     
     slowe_clock_4Hz my_slowe_clock(clk_in, clk_out);
     d_ff d1(clk_out, pb_r, Q);
     d_ff d2(clk_out, Q, Q_n);
 
     assign Q_bar = ~Q_n;
-    assign rect_right = Q & ~Q_bar;
+    assign rect_right = Q & Q_bar;
 
 endmodule
